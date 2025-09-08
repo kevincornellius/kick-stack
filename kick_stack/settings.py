f698dd53cef3cc63528ec2cd42bfd6451be4bd70
Kevin Cornellius Widjaja <kevinbananax@gmail.com>
2025-09-08 22:02:03 +0700

    chore: added allowed hosts for deployment

--- kick_stack/settings.py
+++ kick_stack/settings.py
@@ -32,7 +32,11 @@ PRODUCTION = os.getenv("PRODUCTION", "False").lower() == "true"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "kevin-cornellius-kickstack.pbp.cs.ui.ac.id",
+]
 
 
 # Application definition
