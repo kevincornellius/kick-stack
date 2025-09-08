7a304ab22d70be7d09f608849d73a08b143aa00c
Kevin Cornellius Widjaja <kevinbananax@gmail.com>
2025-09-08 14:05:27 +0700

    feat: Add product listing on view & admin page

--- main/views.py
+++ main/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 
+from main.models import Product
+
 
 # Create your views here.
 def show_main(request):
+    products = Product.objects.all()
     context = {
         "npm": "2406428781",
         "name": "Kevin Cornellius Widjaja",
         "class": "PBP E",
+        "products": products,
     }
 
     return render(request, "main.html", context)
